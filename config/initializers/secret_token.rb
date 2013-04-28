868a56af358f871f25d1ab8dc46eba4d27744783
aurelianonava <sersinfin@gmail.com>
2013-04-27 17:48:36 -0700

    Second commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UshProject::Application.config.secret_token = 'f7f1151ec72ef501b49bb294aee813d474669550a7e116e5641d7096af36a6f0444bc799d2c6680ac10c7bd020563673ca55d3c2131f1b3523c87b58ab387346'
